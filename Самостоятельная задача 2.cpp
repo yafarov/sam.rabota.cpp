382edb56f88ec538d71b27d7be43ff9d2d13c210
yafarov <73663068+yafarov@users.noreply.github.com>
2020-11-14 21:20:01 +0500

    Add files via upload

---
+++ Самостоятельная задача 2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    bool prime = true;
+
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            prime = false;
+            break;
+        }
+        if (prime) {
+            cout << "prime";
+        }
+        else {
+            cout << "composite";
+        }
+        return 0;
+    }
+}
