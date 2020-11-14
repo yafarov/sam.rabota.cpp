f0a9507f20708fb7edba4121beb57776f605937d
yafarov <73663068+yafarov@users.noreply.github.com>
2020-11-14 21:26:11 +0500

    Add files via upload

---
+++ Самостоятельная задача 1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, d, x = 0;
+    cin >> a >> b >> c >> d;
+    for (int i = 10000; i <= 99999; i++)
+        if (i % a == b && i % c == d)
+        {
+            cout << i << ' ';
+            x = 1;
+        }
+    if (x == 0)
+        cout << -1;
+}
